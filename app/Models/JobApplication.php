4600a4e7fa0aa43c540db5bc7d5ded48614a4a4a
DavidBryanCandido <202011090@gordoncollege.edu.ph>
2023-05-14 12:03:41 +0800

    update 1.3
    forgot to add SoftDeletes to models

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobApplication extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'message', 
